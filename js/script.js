6006e891f5392fa7e48e214b17d05f09995a3090
GiuseppeMandriani <giuseppe.mandriani@gmail.com>
2021-04-10 07:46:17 +0200

    add click js

--- js/script.js
+++ js/script.js
@@ -3,6 +3,37 @@ console.log('JQ', $);
 
 $(document).ready(function(){
 
+// Dropdown Menu
+
+// Refs
+
+var dropDown = $('.with-dropdown');
+
+var dropLink = dropDown.find('a');
+
+var dropMenu = dropDown.children('.dropdown-menu');
+
+var dropWindow= dropDown.children('.show');
+
+// Click per sottomenù associato
+
+
+dropLink.click(function(){
+    
+    console.log($(this));
+
+    // Metodo a
+
+    // $(this).next().toggle();
+
+    // Metodo b
+
+    var actualMenu = $(this).next();
+
+    dropMenu.not(actualMenu).hide();
+    dropWindow.not(actualMenu).hide();
+    actualMenu.toggle();
+})
 
 
 
